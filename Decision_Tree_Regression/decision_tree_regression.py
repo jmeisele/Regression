9689a362c55f89fcd6966cdfd6f4d2c6242f02f5
Jason Eisele <jmeisele@yahoo.com>
2018-10-03 13:53:29 -0500

    Deleted comment once again

--- Decision_Tree_Regression/decision_tree_regression.py
+++ Decision_Tree_Regression/decision_tree_regression.py
@@ -14,8 +14,6 @@ y = dataset.iloc[:, 2].values
 """from sklearn.cross_validation import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)"""
 
-#test for github push
-
 # Feature Scaling
 """from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
